f9333d4ddf82d071d817b00cb2a88c9b51e94fec
PabloTerrazas16 <pa.terrazas@duocuc.cl>
2026-06-19 03:54:24 -0400

    Disable discovery and circuit breaker in tests

--- bff/src/test/java/com/bff/ApplicationTests.java
+++ bff/src/test/java/com/bff/ApplicationTests.java
@@ -3,7 +3,10 @@ package com.bff;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.cloud.discovery.enabled=false",
+    "spring.cloud.openfeign.circuitbreaker.enabled=false"
+})
 class ApplicationTests {
 
 	@Test
